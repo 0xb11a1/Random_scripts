2fd4654fadf28fda180a19613bc5a416744c0d5a
theUser <theuser@pop-os.localdomain>
2021-03-27 00:29:37 +0300

    adding some config and gef for gdb

--- init.sh
+++ init.sh
@@ -8,7 +8,8 @@
 
 cd ~ 
 # ---------------------- install essential packages
-sudo apt update && sudo apt install tmux zsh vim curl git xclip -y
+sudo apt update && sudo apt install tmux zsh vim curl git xclip \
+python3-pip python3-dev git libssl-dev libffi-dev build-essential -y
 
 # ----------------------  dracula tmux theme
 
@@ -45,6 +46,11 @@ set -g @dracula-show-location false
 set -g @dracula-show-time false
 set -g @dracula-show-weather false
 set -g @dracula-show-network false
+
+# Set new panes to open in current directory
+bind c new-window -c "#{pane_current_path}"
+bind '"' split-window -c "#{pane_current_path}"
+bind % split-window -h -c "#{pane_current_path}"
 
 # autoinstall 
 if "test ! -d ~/.tmux/plugins/tpm" \
@@ -84,4 +90,8 @@ chsh -s `which zsh`
 
 # ---------------------- install Caskaydia Cove Nerd Font
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip -O /tmp/CascadiaCode.zip
-mkdir -p ~/.local/share/fonts ;  unzip /tmp/CascadiaCode.zip Caskaydia\ Cove\ Nerd\ Font\ Complete.ttf -d ~/.local/share/fonts
+mkdir ~/.local/share/fonts ;  unzip /tmp/CascadiaCode.zip Caskaydia\ Cove\ Nerd\ Font\ Complete.ttf -d ~/.local/share/fonts
+
+# ---------------------- install gef in gdb 
+wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
+echo source ~/.gdbinit-gef.py >> ~/.gdbinit
